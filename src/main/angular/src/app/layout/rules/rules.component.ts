290e85332c28f7a3478caeaea35f4b1a2d67640a
Csókási Marcell <csokasi.marcell@gmail.com>
2020-09-21 21:29:26 +0200

    finishing rules logic

--- src/main/angular/src/app/layout/rules/rules.component.ts
+++ src/main/angular/src/app/layout/rules/rules.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ArticleType} from "../../shared/enums/article-type.enum";
+import {filter, map} from "rxjs/operators";
+import {HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {Article} from "../../shared/domain/article";
+import {ArticleService} from "../../shared/service/article.service";
 
 @Component({
   selector: 'app-rules',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RulesComponent implements OnInit {
 
-  constructor() { }
+  contentNotFound = true;
+  rulesArticle: Article;
+
+  constructor(private readonly articleService: ArticleService) { }
 
   ngOnInit() {
+    this.loadAll();
   }
 
+  loadAll() {
+    this.articleService
+      .query({type: ArticleType.RULES})
+      .pipe(
+        filter((res: HttpResponse<Article[]>) => res.ok),
+        map((res: HttpResponse<Article[]>) => res.body)
+      )
+      .subscribe(
+        (res: Article[]) => {
+          if(res.length === 0)
+          {
+            this.contentNotFound = true;
+          } else {
+            this.contentNotFound = false;
+            this.rulesArticle = res[0];
+          }
+        },
+        () => this.onError()
+      );
+  }
+
+  private onError() {
+      this.contentNotFound = true;
+      this.rulesArticle = null;
+  }
 }
