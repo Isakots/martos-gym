ee7e67c14917fae0f5b2d15fd43a1a155c959e69
Csókási Marcell <csokasi.marcell@gmail.com>
2020-09-21 21:29:13 +0200

    finishing gym-info logic

--- src/main/angular/src/app/layout/gym-info/gym-info.component.ts
+++ src/main/angular/src/app/layout/gym-info/gym-info.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Article} from "../../shared/domain/article";
+import {ArticleService} from "../../shared/service/article.service";
+import {ArticleType} from "../../shared/enums/article-type.enum";
+import {filter, map} from "rxjs/operators";
+import {HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-gym-info',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GymInfoComponent implements OnInit {
 
-  constructor() { }
+  contentNotFound = true;
+  gymInfoArticle: Article;
+
+  constructor(private readonly articleService: ArticleService) { }
 
   ngOnInit() {
+    this.loadAll();
+  }
+
+  loadAll() {
+    this.articleService
+      .query({type: ArticleType.GYM})
+      .pipe(
+        filter((res: HttpResponse<Article[]>) => res.ok),
+        map((res: HttpResponse<Article[]>) => res.body)
+      )
+      .subscribe(
+        (res: Article[]) => {
+          if(res.length === 0)
+          {
+            this.contentNotFound = true;
+          } else {
+            this.contentNotFound = false;
+            this.gymInfoArticle = res[0];
+          }
+        },
+        () => this.onError()
+      );
+  }
+
+  private onError() {
+    this.contentNotFound = true;
+    this.gymInfoArticle = null;
   }
 
 }
