d30463dea4143ae9060ccebce15aecafb24a749b
csokasi.marcell <csokasi.marcell@gmail.com>
2020-10-17 15:01:46 +0200

    forcing user-fetch on page reload

--- src/main/angular/src/app/app.component.ts
+++ src/main/angular/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from "./shared/service/account.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(
+    private accountService: AccountService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.accountService.identity(true);
+  }
 }
